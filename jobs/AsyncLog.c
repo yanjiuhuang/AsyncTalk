94b6cc19ea03b5fdcdb25e0e3663b663509757e4
Yanjiu Huang <yhuang@zynga.com>
2014-03-24 18:17:48 +0800

    Add the Async Log implementation

--- jobs/AsyncLog.c
+++ jobs/AsyncLog.c
@@ -7,17 +7,30 @@
 //
 
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "AsyncLog.h"
 
-void async_log_error(const char* format,...){
-   
+void async_log_error(const char* format, ...){
+    va_list arg;
+    
+    va_start(arg, format);
+    vfprintf(stderr, format, arg);
+    va_end(arg);
 }
 
-void async_log_debug(const char* format,...){
-
+void async_log_debug(const char* format, ...){
+    va_list arg;
+    
+    va_start(arg, format);
+    vfprintf(stdout, format, arg);
+    va_end(arg);
 }
 
-void async_log_info(const char* format,...){
-
+void async_log_info(const char* format, ...){
+    va_list arg;
+    
+    va_start(arg, format);
+    vfprintf(stdout, format, arg);
+    va_end(arg);
 }
